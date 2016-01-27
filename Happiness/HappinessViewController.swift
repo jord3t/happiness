c9a4d3e719d73b9cb48a88e770b371fac4dc6df1
trevor <trevor.jordet@gmail.com>
2016-01-27 14:51:38 -0500

    base smiley added

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by trevor jordet on 1/27/16.
+//  Copyright (c) 2016 jord3t. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
