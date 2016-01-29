e60f19c1e0fdd8b0a8bb0b242e5442a716e37a19
trevor <trevor.jordet@gmail.com>
2016-01-29 16:29:08 -0500

    pan gesture added

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class HappinessViewController: UIViewController, FaceViewDataSource
 {
+    var happiness: Int = 75 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
     
     @IBOutlet weak var faceView: FaceView! {
         didSet {
@@ -19,13 +26,24 @@ class HappinessViewController: UIViewController, FaceViewDataSource
         }
     }
     
-    var happiness: Int = 75 {
-        didSet {
-            happiness = min(max(happiness, 0), 100)
-            println("happiness = \(happiness)")
-            //updateUI()
+    private struct Constants {
+    static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange =  -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
         }
     }
+    
     
     private func updateUI()
     {
